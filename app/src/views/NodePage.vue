9878a4eebf86182b9f1a2af64a941485a4ccc11c
Foxinou <37015399+FoxinouFR@users.noreply.github.com>
2025-09-14 01:20:19 +0200

    refactor: update import paths and replace IonIcon with FontAwesome icons in NodePage.vue

--- app/src/views/NodePage.vue
+++ app/src/views/NodePage.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
 import {
-	IonPage, IonTabs, IonRouterOutlet,
-	IonTabBar, IonTabButton, IonIcon, IonLabel
-} from '@ionic/vue';
-import { shieldHalf, settings, wallet, power, helpCircle } from 'ionicons/icons';
+	IonPage, IonTabs,
+	IonTabBar, IonTabButton, IonLabel
+} from '@/ui';
 import { startNetworkMonitorHook } from '@/hooks/NetworkMonitorHook';
 
 // Start the network monitor hook
@@ -11,38 +10,31 @@ startNetworkMonitorHook();
 </script>
 
 <template>
-	<ion-page>
-		<ion-tabs>
-			<ion-router-outlet></ion-router-outlet>
-			<ion-tabs>
-				<ion-router-outlet></ion-router-outlet>
-				<ion-tab-bar slot="bottom">
-					<ion-tab-button tab="Dashboard" href="/node/dashboard">
-						<ion-icon :icon="shieldHalf" />
-						<ion-label>{{ $t('app.tab-dashboard') }}</ion-label>
-					</ion-tab-button>
-
-					<ion-tab-button tab="Settings" href="/node/settings">
-						<ion-icon :icon="settings" />
-						<ion-label>{{ $t('app.tab-settings') }}</ion-label>
-					</ion-tab-button>
-
-					<ion-tab-button tab="Wallet" href="/node/wallet">
-						<ion-icon :icon="wallet" />
-						<ion-label>{{ $t('app.tab-wallet') }}</ion-label>
-					</ion-tab-button>
-
-					<ion-tab-button tab="Actions" href="/node/actions">
-						<ion-icon :icon="power" />
-						<ion-label>{{ $t('app.tab-actions') }}</ion-label>
-					</ion-tab-button>
-
-					<ion-tab-button tab="About" href="/node/about">
-						<ion-icon :icon="helpCircle" />
-						<ion-label>{{ $t('app.tab-about') }}</ion-label>
-					</ion-tab-button>
-				</ion-tab-bar>
-			</ion-tabs>
-		</ion-tabs>
-	</ion-page>
+<ion-page>
+	<div class="ion-tabs">
+		<router-view />
+		<nav class="ion-tab-bar">
+			<router-link class="ion-tab-button" :to="{ name: 'NodeDashboard' }">
+				<font-awesome-icon :icon="['fas','shield-halved']" />
+				<ion-label>{{ $t('app.tab-dashboard') }}</ion-label>
+			</router-link>
+			<router-link class="ion-tab-button" :to="{ name: 'NodeSettings' }">
+				<font-awesome-icon :icon="['fas','gear']" />
+				<ion-label>{{ $t('app.tab-settings') }}</ion-label>
+			</router-link>
+			<router-link class="ion-tab-button" :to="{ name: 'NodeWallet' }">
+				<font-awesome-icon :icon="['fas','wallet']" />
+				<ion-label>{{ $t('app.tab-wallet') }}</ion-label>
+			</router-link>
+			<router-link class="ion-tab-button" :to="{ name: 'NodeActions' }">
+				<font-awesome-icon :icon="['fas','power-off']" />
+				<ion-label>{{ $t('app.tab-actions') }}</ion-label>
+			</router-link>
+			<router-link class="ion-tab-button" :to="{ name: 'NodeAbout' }">
+				<font-awesome-icon :icon="['fas','circle-question']" />
+				<ion-label>{{ $t('app.tab-about') }}</ion-label>
+			</router-link>
+		</nav>
+	</div>
+</ion-page>
 </template>
