2238d5e2a17eaa88e1c299984cb3448c1a487f1b
Foxinou <37015399+FoxinouFR@users.noreply.github.com>
2025-09-14 01:23:59 +0200

    refactor: update import path for Ion components and improve template structure in Step10Congratulations.vue

--- app/src/views/Wizard/Step10Congratulations.vue
+++ app/src/views/Wizard/Step10Congratulations.vue
@@ -2,7 +2,7 @@
 import {
 	IonPage, IonContent,
 	IonGrid, IonCol, IonRow
-} from '@ionic/vue';
+} from '@/ui';
 import { useRouter } from 'vue-router';
 import NodeService from '@/services/NodeService';
 import LoadingButton from '@components/LoadingButton.vue';
@@ -24,26 +24,26 @@ const refreshStoreAndNavigate = async () =>
 
 </script>
 <template>
-	<ion-page>
-		<ion-content :fullscreen="true">
-			<div class="wizard">
-				<div class="form">
-					<h1>{{ $t('wizard.congratulations-title') }}</h1>
-					<p class="text">{{ $t('wizard.congratulations-text') }}</p>
-					<p class="text">{{ $t('wizard.congratulations-next') }}</p>
-				</div>
-				<div class="submit">
-					<ion-grid>
-						<ion-row>
-							<ion-col size="6" offset="6">
-								<loading-button :label="$t('wizard.button-finish')" :callback="refreshStoreAndNavigate" />
-							</ion-col>
-						</ion-row>
-					</ion-grid>
-				</div>
+<ion-page>
+	<ion-content :fullscreen="true">
+		<div class="wizard">
+			<div class="form">
+				<h1>{{ $t('wizard.congratulations-title') }}</h1>
+				<p class="text">{{ $t('wizard.congratulations-text') }}</p>
+				<p class="text">{{ $t('wizard.congratulations-next') }}</p>
 			</div>
-		</ion-content>
-	</ion-page>
+			<div class="submit">
+				<ion-grid>
+					<ion-row>
+						<ion-col size="6" offset="6">
+							<loading-button :label="$t('wizard.button-finish')" :callback="refreshStoreAndNavigate" />
+						</ion-col>
+					</ion-row>
+				</ion-grid>
+			</div>
+		</div>
+	</ion-content>
+</ion-page>
 </template>
 <style lang="scss" scoped>
 @import "@scss/wizard.scss";
