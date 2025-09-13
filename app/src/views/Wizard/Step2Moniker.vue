c7610d11bdc8bedadaa75251e0de10f4f4458d47
Foxinou <37015399+FoxinouFR@users.noreply.github.com>
2025-09-14 01:23:24 +0200

    refactor: update import path for Ion components and improve template structure in Step2Moniker.vue

--- app/src/views/Wizard/Step2Moniker.vue
+++ app/src/views/Wizard/Step2Moniker.vue
@@ -5,7 +5,7 @@ import
 	IonPage, IonContent,
 	IonGrid, IonRow, IonCol,
 	IonList, IonItem, IonText,
-} from '@ionic/vue';
+} from '@/ui';
 import { useRouter } from 'vue-router';
 import { useI18n } from 'vue-i18n';
 import { useNodeStore } from '@stores/NodeStore';
@@ -64,41 +64,40 @@ const setValueAndNavigate = async () =>
 
 </script>
 <template>
-	<ion-page>
-		<ion-content :fullscreen="true">
-			<div class="wizard">
-				<div class="form">
-					<h1>{{ $t('wizard.moniker-title') }}</h1>
-					<p class="text">{{ $t('wizard.moniker-text') }}</p>
-					<p class="text">{{ $t('wizard.moniker-explain') }}</p>
-					<ion-list class="input">
-						<ion-item lines="none">
-							<app-input
-								:placeholder="$t('wizard.moniker-placeholder')"
-								:errorMessage="$t('wizard.moniker-error')"
-								type="text"
-								v-model="moniker"
-								aria-label="Moniker"
-								:minLength="4"
-							/>
-						</ion-item>
-						<ion-item lines="none" v-if="errorMessage">
-							<ion-text color="danger">{{ errorMessage }}</ion-text>
-						</ion-item>
-					</ion-list>
-				</div>
-				<div class="submit">
-					<ion-grid>
-						<ion-row>
-							<ion-col size="6" offset="6">
-								<loading-button :label="$t('wizard.button-next')" :callback="setValueAndNavigate" />
-							</ion-col>
-						</ion-row>
-					</ion-grid>
-				</div>
+<ion-page>
+	<ion-content :fullscreen="true">
+		<div class="wizard">
+			<div class="form">
+				<h1>{{ $t('wizard.moniker-title') }}</h1>
+				<p class="text">{{ $t('wizard.moniker-text') }}</p>
+				<p class="text">{{ $t('wizard.moniker-explain') }}</p>
+				<ion-list class="input">
+					<ion-item lines="none">
+						<app-input
+							:placeholder="$t('wizard.moniker-placeholder')"
+							:errorMessage="$t('wizard.moniker-error')"
+							type="text"
+							v-model="moniker"
+							aria-label="Moniker"
+							:minLength="4"/>
+					</ion-item>
+					<ion-item lines="none" v-if="errorMessage">
+						<ion-text color="danger">{{ errorMessage }}</ion-text>
+					</ion-item>
+				</ion-list>
 			</div>
-		</ion-content>
-	</ion-page>
+			<div class="submit">
+				<ion-grid>
+					<ion-row>
+						<ion-col size="6" offset="6">
+							<loading-button :label="$t('wizard.button-next')" :callback="setValueAndNavigate" />
+						</ion-col>
+					</ion-row>
+				</ion-grid>
+			</div>
+		</div>
+	</ion-content>
+</ion-page>
 </template>
 <style lang="scss" scoped>
 @import "@scss/wizard.scss";
