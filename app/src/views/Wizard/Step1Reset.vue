52c097999ab4bb889250197c9d95d89ec2f5b95d
Foxinou <37015399+FoxinouFR@users.noreply.github.com>
2025-09-14 01:23:44 +0200

    refactor: update import path for Ion components and improve template structure in Step1Reset.vue

--- app/src/views/Wizard/Step1Reset.vue
+++ app/src/views/Wizard/Step1Reset.vue
@@ -4,7 +4,7 @@ import {
 	IonPage, IonContent, IonButton,
 	IonGrid, IonCol, IonRow,
 	IonItem, IonText,
-} from '@ionic/vue';
+} from '@/ui';
 import { onMounted } from 'vue';
 import { useRouter } from 'vue-router';
 import { useI18n } from 'vue-i18n';
@@ -47,37 +47,37 @@ const resetConfiguration = async () =>
 
 </script>
 <template>
-	<ion-page>
-		<ion-content :fullscreen="true">
-			<div class="wizard">
-				<div class="form">
-					<h1>{{ $t('wizard.reset-title') }}</h1>
-					<p class="text">{{ $t('wizard.reset-text') }}</p>
-					<p class="text">{{ $t('wizard.reset-question') }}</p>
-				</div>
-				<div class="submit">
-					<ion-grid>
-						<ion-row>
-							<ion-item lines="none" v-if="errorMessage">
-								<ion-text color="danger">{{ errorMessage }}</ion-text>
-							</ion-item>
-						</ion-row>
-						<ion-row>
-							<ion-col size="6">
-								<loading-button :label="$t('wizard.button-yes')" :callback="resetConfiguration" />
-							</ion-col>
-							<ion-col size="6">
-								<ion-button expand="block" :router-link="{ name: 'Wizard2Moniker' }"
-									router-direction="forward">
-									{{ $t('wizard.button-no') }}
-								</ion-button>
-							</ion-col>
-						</ion-row>
-					</ion-grid>
-				</div>
+<ion-page>
+	<ion-content :fullscreen="true">
+		<div class="wizard">
+			<div class="form">
+				<h1>{{ $t('wizard.reset-title') }}</h1>
+				<p class="text">{{ $t('wizard.reset-text') }}</p>
+				<p class="text">{{ $t('wizard.reset-question') }}</p>
 			</div>
-		</ion-content>
-	</ion-page>
+			<div class="submit">
+				<ion-grid>
+					<ion-row>
+						<ion-item lines="none" v-if="errorMessage">
+							<ion-text color="danger">{{ errorMessage }}</ion-text>
+						</ion-item>
+					</ion-row>
+					<ion-row>
+						<ion-col size="6">
+							<loading-button :label="$t('wizard.button-yes')" :callback="resetConfiguration" />
+						</ion-col>
+						<ion-col size="6">
+							<ion-button expand="block" :router-link="{ name: 'Wizard2Moniker' }"
+								router-direction="forward">
+								{{ $t('wizard.button-no') }}
+							</ion-button>
+						</ion-col>
+					</ion-row>
+				</ion-grid>
+			</div>
+		</div>
+	</ion-content>
+</ion-page>
 </template>
 <style lang="scss" scoped>
 @import "@scss/wizard.scss";
