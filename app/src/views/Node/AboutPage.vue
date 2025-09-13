ce432761a94fa292a493a48a8c98fc41a47bd22f
Foxinou <37015399+FoxinouFR@users.noreply.github.com>
2025-09-14 01:19:41 +0200

    refactor: update import path for IonButton and replace IonIcon with FontAwesome icons in AboutPage.vue

--- app/src/views/Node/AboutPage.vue
+++ app/src/views/Node/AboutPage.vue
@@ -2,9 +2,8 @@
 import {
 	IonPage, IonContent, IonHeader,
 	IonCard, IonCardHeader, IonCardTitle, IonCardContent,
-	IonIcon, IonButton
-} from '@ionic/vue';
-import { document } from 'ionicons/icons';
+	IonButton
+} from '@/ui';
 import { ref, onMounted, onUnmounted } from 'vue';
 import AppToolbar from '@/components/AppToolbar.vue';
 import { useNodeStore } from '@stores/NodeStore';
@@ -64,70 +63,70 @@ onUnmounted(() =>
 
 </script>
 <template>
-	<ion-page>
-		<ion-header>
-			<app-toolbar />
-		</ion-header>
-		<ion-content>
-			<div class="about">
-				<ion-card class="container">
-					<ion-card-header>
-						<ion-card-title>{{ $t('about.app-information-title') }}</ion-card-title>
-					</ion-card-header>
-					<ion-card-content>
-						<p class="item">
-							<strong>{{ $t('about.app-version-label') }}</strong>{{ buildInfo.version }}
-						</p>
-						<p class="item">
-							<strong>{{ $t('about.app-release-date-label') }}</strong>{{ buildInfo.buildDate }}
-						</p>
-					</ion-card-content>
-				</ion-card>
+<ion-page>
+	<ion-header>
+		<app-toolbar />
+	</ion-header>
+	<ion-content>
+		<div class="about">
+			<ion-card class="container">
+				<ion-card-header>
+					<ion-card-title>{{ $t('about.app-information-title') }}</ion-card-title>
+				</ion-card-header>
+				<ion-card-content>
+					<p class="item">
+						<strong>{{ $t('about.app-version-label') }}</strong>{{ buildInfo.version }}
+					</p>
+					<p class="item">
+						<strong>{{ $t('about.app-release-date-label') }}</strong>{{ buildInfo.buildDate }}
+					</p>
+				</ion-card-content>
+			</ion-card>
 
-				<ion-card class="container">
-					<ion-card-header>
-						<ion-card-title>{{ $t('about.system-information-title') }}</ion-card-title>
-					</ion-card-header>
-					<ion-card-content>
-						<p class="item">
-							<strong>{{ $t('about.casanode-version-label') }}</strong>{{ nodeStore.casanodeVersion }}
-						</p>
-						<p class="item">
-							<strong>{{ $t('about.operating-system-label') }}</strong>{{ nodeStore.systemOs }}
-						</p>
-						<p class="item">
-							<strong>{{ $t('about.kernel-version-label') }}</strong>{{ nodeStore.systemKernel }}
-						</p>
-						<p class="item">
-							<strong>{{ $t('about.architecture-label') }}</strong>{{ nodeStore.systemArch }}
-						</p>
-						<p class="item">
-							<strong>{{ $t('about.docker-image-label') }}</strong>{{ nodeStore.dockerImage }}
-						</p>
-						<p class="item">
-							<strong>{{ $t('about.uptime-label') }}</strong>{{ uptime }}
-						</p>
-					</ion-card-content>
-				</ion-card>
+			<ion-card class="container">
+				<ion-card-header>
+					<ion-card-title>{{ $t('about.system-information-title') }}</ion-card-title>
+				</ion-card-header>
+				<ion-card-content>
+					<p class="item">
+						<strong>{{ $t('about.casanode-version-label') }}</strong>{{ nodeStore.casanodeVersion }}
+					</p>
+					<p class="item">
+						<strong>{{ $t('about.operating-system-label') }}</strong>{{ nodeStore.systemOs }}
+					</p>
+					<p class="item">
+						<strong>{{ $t('about.kernel-version-label') }}</strong>{{ nodeStore.systemKernel }}
+					</p>
+					<p class="item">
+						<strong>{{ $t('about.architecture-label') }}</strong>{{ nodeStore.systemArch }}
+					</p>
+					<p class="item">
+						<strong>{{ $t('about.docker-image-label') }}</strong>{{ nodeStore.dockerImage }}
+					</p>
+					<p class="item">
+						<strong>{{ $t('about.uptime-label') }}</strong>{{ uptime }}
+					</p>
+				</ion-card-content>
+			</ion-card>
 
-				<ion-card class="container">
-					<ion-card-header>
-						<ion-card-title>{{ $t('about.legal-title') }}</ion-card-title>
-					</ion-card-header>
-					<ion-card-content>
-						<ion-button fill="outline" target="_system" href="https://www.sentinel.co/terms-of-service">
-							<ion-icon :icon="document"></ion-icon>
-							{{ $t('about.terms-of-service') }}
-						</ion-button>
-						<ion-button fill="outline" target="_system" href="https://www.sentinel.co/privacy-policy">
-							<ion-icon :icon="document"></ion-icon>
-							{{ $t('about.privacy-policy') }}
-						</ion-button>
-					</ion-card-content>
-				</ion-card>
-			</div>
-		</ion-content>
-	</ion-page>
+			<ion-card class="container">
+				<ion-card-header>
+					<ion-card-title>{{ $t('about.legal-title') }}</ion-card-title>
+				</ion-card-header>
+				<ion-card-content>
+					<ion-button fill="outline" target="_system" href="https://www.sentinel.co/terms-of-service">
+						<font-awesome-icon :icon="['fas','file-lines']" />
+						{{ $t('about.terms-of-service') }}
+					</ion-button>
+					<ion-button fill="outline" target="_system" href="https://www.sentinel.co/privacy-policy">
+						<font-awesome-icon :icon="['fas','file-lines']" />
+						{{ $t('about.privacy-policy') }}
+					</ion-button>
+				</ion-card-content>
+			</ion-card>
+		</div>
+	</ion-content>
+</ion-page>
 </template>
 <style lang="scss" scoped>
 @import "@scss/container.scss";
