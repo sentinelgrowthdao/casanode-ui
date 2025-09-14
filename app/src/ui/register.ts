e9e6e9a14801005985b6ef262415f9a84c11dcb0
Foxinou <37015399+FoxinouFR@users.noreply.github.com>
2025-09-14 23:40:51 +0200

    style: format code for consistency in registerUi function

--- app/src/ui/register.ts
+++ app/src/ui/register.ts
@@ -9,13 +9,13 @@ import IonCardContent from './IonCardContent.vue';
 
 export function registerUi(app: App) 
 {
-  app.component('ion-page', IonPage);
-  app.component('ion-header', IonHeader);
-  app.component('ion-content', IonContent);
-  app.component('ion-button', IonButton);
-  app.component('ion-card-header', IonCardHeader);
-  app.component('ion-card-title', IonCardTitle);
-  app.component('ion-card-content', IonCardContent);
+	app.component('ion-page', IonPage);
+	app.component('ion-header', IonHeader);
+	app.component('ion-content', IonContent);
+	app.component('ion-button', IonButton);
+	app.component('ion-card-header', IonCardHeader);
+	app.component('ion-card-title', IonCardTitle);
+	app.component('ion-card-content', IonCardContent);
 }
 
 export { IonPage, IonHeader, IonContent, IonButton, IonCardHeader, IonCardTitle, IonCardContent };
