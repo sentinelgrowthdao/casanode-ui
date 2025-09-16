4262f734bf35150f717d85fe56ff18d40b5e8f29
Foxinou <37015399+FoxinouFR@users.noreply.github.com>
2025-09-16 20:41:23 +0200

    refactor: remove app information section from AboutPage and localization files

--- app/src/views/Node/AboutPage.vue
+++ app/src/views/Node/AboutPage.vue
@@ -7,7 +7,6 @@ import {
 import { ref, onMounted, onUnmounted } from 'vue';
 import AppToolbar from '@/components/AppToolbar.vue';
 import { useNodeStore } from '@stores/NodeStore';
-import buildInfo from '@/assets/build.json';
 
 // Import the useNodeStore composable function.
 const nodeStore = useNodeStore();
@@ -69,20 +68,6 @@ onUnmounted(() =>
 	</ion-header>
 	<ion-content>
 		<div class="about">
-			<ion-card class="container">
-				<ion-card-header>
-					<ion-card-title>{{ $t('about.app-information-title') }}</ion-card-title>
-				</ion-card-header>
-				<ion-card-content>
-					<p class="item">
-						<strong>{{ $t('about.app-version-label') }}</strong>{{ buildInfo.version }}
-					</p>
-					<p class="item">
-						<strong>{{ $t('about.app-release-date-label') }}</strong>{{ buildInfo.buildDate }}
-					</p>
-				</ion-card-content>
-			</ion-card>
-
 			<ion-card class="container">
 				<ion-card-header>
 					<ion-card-title>{{ $t('about.system-information-title') }}</ion-card-title>
